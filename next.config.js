4e714be71342a80a32475d6b970d21755ad724db
Mitsunee <michelle@mitsunee.com>
2021-09-07 13:09:48 +0200

    update markdown parser with new plugins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,51 @@
-const withTM = require("next-transpile-modules")(["nanostores"]);
+const nextTranspileModules = require("next-transpile-modules");
 const chalk = require("chalk");
+
+const esModulePackages = [
+  // nanostores:
+  "nanostores",
+  // unified, remark, rehype
+  "unified",
+  "remark-parse",
+  "remark-unwrap-images",
+  "remark-rehype",
+  "rehype-slug",
+  "rehype-external-links",
+  "rehype-stringify",
+  // dependencies of above
+  "bail",
+  "ccount",
+  "character-entities",
+  "comma-separated-tokens",
+  "hast-util-has-property",
+  "hast-util-heading-rank",
+  "hast-util-is-element",
+  "hast-util-to-html",
+  "hast-util-to-string",
+  "hast-util-whitespace",
+  "html-void-elements",
+  "is-absolute-url",
+  "is-plain-obj",
+  "mdast-util-definitions",
+  "mdast-util-from-markdown",
+  "mdast-util-to-hast",
+  "mdast-util-to-string",
+  "micromark",
+  "parse-entities",
+  "property-information",
+  "space-separated-tokens",
+  "stringify-entities",
+  "trough",
+  "unist-builder",
+  "unist-util-generated",
+  "unist-util-is",
+  "unist-util-position",
+  "unist-util-stringify-position",
+  "unist-util-visit",
+  "vfile"
+];
+
+const withTM = nextTranspileModules(esModulePackages);
 
 console.log(`${chalk.cyan("info")}  - React StrictMode is enabled`);
 
--- src/utils/parseMarkdown.js
+++ src/utils/parseMarkdown.js
@@ -1,7 +1,21 @@
-import remark from "remark";
-import html from "remark-html";
-import prism from "remark-prism";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkPrism from "remark-prism";
+import remarkUnwrapImages from "remark-unwrap-images";
+import remarkToRehype from "remark-rehype";
+import rehypeSlug from "rehype-slug";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeToString from "rehype-stringify";
+
+const parser = unified()
+  .use(remarkParse)
+  .use(remarkPrism)
+  .use(remarkUnwrapImages)
+  .use(remarkToRehype)
+  .use(rehypeSlug)
+  .use(rehypeExternalLinks)
+  .use(rehypeToString);
 
 export default async function parseMarkdown(markdown) {
-  return await remark().use(html).use(prism).process(markdown);
+  return await parser.process(markdown);
 }
