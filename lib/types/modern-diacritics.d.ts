84f3dcc61a073a98f4494fe103620ea6801ac0ff
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-07-15 15:05:25 +0200

    upgrade modern-diacritics and remove type overrides

--- lib/types/modern-diacritics.d.ts
+++
@@ -1,9 +0,0 @@
-// TODO: copy remaining declarations as needed
-
-declare module "modern-diacritics" {
-  interface SlugifyOptions {
-    forceSingleSpace?: boolean;
-    trim?: boolean;
-  }
-  function slugify(str: string, options?: SlugifyOptions): string;
-}
