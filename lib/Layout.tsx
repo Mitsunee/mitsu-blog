e0e23593dd6ceb41bc283b7b2e36904e4bc81d06
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-07-18 12:51:34 +0200

    add: navigation in header

--- lib/Layout.tsx
+++ lib/Layout.tsx
@@ -1,17 +1,37 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { PropsWithChildren } from "react";
+import cc from "classcat";
 
-// TODO: implement navbar with text link to Home and a search bar when /search page is implemented
+import styles from "./Layout.module.css";
 
 export function Layout({ children }: PropsWithChildren) {
+  const router = useRouter();
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <Link href="/">
-          <a title="Mitsunee | Blog">
+          <a title="Mitsunee | Blog" className={styles.logo}>
             <img alt="Mitsunee" src="/assets/icons/icon.svg" />
           </a>
         </Link>
+        <ul className={styles.links} role="navigation" aria-label="Main">
+          <li>
+            <Link href="/">
+              <a className={cc([router.asPath == "/" && styles.curr])}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/posts/1">
+              <a
+                className={cc([
+                  router.asPath.startsWith("/posts/") && styles.curr
+                ])}>
+                All Posts
+              </a>
+            </Link>
+          </li>
+        </ul>
       </header>
       <main>{children}</main>
     </>
