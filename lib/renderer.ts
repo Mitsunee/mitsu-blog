9a3329bb32d116e523def9349886920b1a22ae6e
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-11-09 14:57:37 +0100

    remove tsignore comment in renderer

--- lib/renderer.ts
+++ lib/renderer.ts
@@ -7,15 +7,11 @@ import { Section } from "./Section";
 export function renderer(content: string) {
   const rendered = rehype()
     .data("settings", { fragment: true })
-    .use(
-      rehypeReact,
-      // @ts-ignore: the types of rehype-react love to not agree, don't care as long as it works
-      {
-        createElement,
-        Fragment,
-        components: { a: AutoLink, section: Section }
-      }
-    )
+    .use(rehypeReact, {
+      createElement,
+      Fragment,
+      components: { a: AutoLink, section: Section }
+    })
     .processSync(content);
 
   return rendered.result;
